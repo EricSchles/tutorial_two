84b149ab459c21c616648003dce8922e20f73d9c
EricSchles <ericschles@gmail.com>
2015-01-02 13:10:21 -0800

    lesson five started

---
+++ interfacing.java
@@ -0,0 +1,9 @@
+public interface interfacing{
+	void hello(String x);
+	//prints hello + x
+	void print(input);
+	//wraps System.out.println
+	Boolean rabbies(Object obj);
+	//check if obj has .rabbies property.  if set to true, return true
+	//else return false.
+}
---
+++ lesson_five.java
@@ -0,0 +1,11 @@
+/*
+how to compile:
+javac lesson_five.java interfacing.java
+java lesson_five
+
+Explanation:
+
+*/
+class lesson_five implements interfacing {
+
+}
