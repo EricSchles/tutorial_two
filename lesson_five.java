ec0a2af543e1d9304528be7da8d4a0a8dc68d27f
EricSchles <ericschles@gmail.com>
2015-01-03 04:50:18 -0800

    lesson five

--- lesson_five.java
+++ lesson_five.java
@@ -5,7 +5,30 @@ java lesson_five
 
 Explanation:
 
-*/
-class lesson_five implements interfacing {
+Interfaces are useful for prototyping code and also for making sure someone else
+writes all the code they are supposed to, in order to use other code.
 
+For instance, say you wrote something that assumes every object you pass around has a toString method
+or a has_cats method.  If you pass in an object that doesn't have such a method, the code may fail.
+
+By forcing all objects passed in to conform to an interface, it will always be clear what methods or variables are
+required.  
+
+This isn't particularly useful when you are working by yourself, however when you work with others and particularly on
+large scale projects, this is extremely useful.  Of course we could all just be good communicators, but that doesn't
+really tend to happen.
+
+*/
+class implementar implements interfacing {
+	void hello(String x);
+	//prints hello + x
+	void print(input);
+	//wraps System.out.println
+	Boolean rabbies(Object obj);
+	//check if obj has .rabbies property.  if set to true, return true
+	//else return false.
+}
+class lesson_five{
+
+	
 }
