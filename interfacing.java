ad1d6c183ff0cf66a9c663d79a7946e73c07560a
EricSchles <ericschles@gmail.com>
2015-01-03 04:56:33 -0800

    trying things

--- interfacing.java
+++ interfacing.java
@@ -1,9 +1,9 @@
 public interface interfacing{
-	void hello(String x);
+	public void hello(String x);
 	//prints hello + x
-	<T> void print(T input);
+	public static <T> void print(T input);
 	//wraps System.out.println
-	Boolean rabbies(Object obj);
+	public Boolean rabbies(Object obj);
 	//check if obj has .rabbies property.  if set to true, return true
 	//else return false.
 	
